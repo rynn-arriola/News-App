92789919df804e5fbb642708b92cce15bb581587
rynn-arriola <rynn.arriola@digital-tectonics.com>
2023-09-16 09:28:45 -0400

    create TopHeadLinesFragment xml

--- app/src/main/java/com/example/rynnarriola/newsapp/ui/fragments/TopHeadLinesFragment.kt
+++ app/src/main/java/com/example/rynnarriola/newsapp/ui/fragments/TopHeadLinesFragment.kt
@@ -1,6 +1,23 @@
 package com.example.rynnarriola.newsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.rynnarriola.newsapp.databinding.FragmentTopHeadlinesBinding
 
 class TopHeadLinesFragment : Fragment() {
+
+    private var _binding: FragmentTopHeadlinesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentTopHeadlinesBinding.inflate(inflater, container, false).also {
+        _binding = it
+
+    }.root
 }
