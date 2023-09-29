f154ab0922e2905d8714bf79f541bd0e6685507e
fakexaxa <fakexaxa@yahoo.com>
2023-09-29 14:54:31 -0400

    set up news source UI

--- app/src/main/java/com/example/rynnarriola/newsapp/ui/fragments/NewsSourcesFragment.kt
+++ app/src/main/java/com/example/rynnarriola/newsapp/ui/fragments/NewsSourcesFragment.kt
@@ -1,6 +1,22 @@
 package com.example.rynnarriola.newsapp.ui.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.rynnarriola.newsapp.databinding.FragmentNewsSourceBinding
 
 class NewsSourcesFragment : Fragment() {
+
+    private var _binding: FragmentNewsSourceBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = FragmentNewsSourceBinding.inflate(inflater, container, false).also {
+        _binding = it
+
+    }.root
 }
